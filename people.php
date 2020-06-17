cf20257fb94c157da6a35ddf639dd60b3fa2330e
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-17 10:47:44 -0400

    Add rendering of people from csv file.

--- people.php
+++ people.php
@@ -5,21 +5,47 @@
 $title = 'People Recommendations';
 require 'inc/header.php';
 
-echo '<pre>';
+/* echo '<pre>';   // pre tag renders the element contents exactly as written in the source code
 include 'data/csv/people.csv';
-echo '</pre>';
+echo '</pre>'; */
 
-echo '<div class="row">';
+// open the csv file for reading
+$csvFileHandler = fopen('data/csv/people.csv', 'r');
+if ($csvFileHandler) {
+    // read each line as a row of columns per csv format and return an array of columns
+    $header = fgetcsv($csvFileHandler);  // ',' is the defaul delimeter
+    // flip the array so the header values are the keys and extract each key
+    // into its own variable named after the key
+    // this is so the values of the keys represent the position in each record's array
+    // then we can use the key to reference the data for rendering for ease of code readability
+    // e.g. $contact[$first] = $contact[0] = first name of record
+    extract(array_flip($header));
+    // loop through the remaining lines in the csv file to display the contacts
+    echo '<div class="row">';   // the loop is inside the 'row' div so there's always at least one row
+    $count = 0;
+    while (!feof($csvFileHandler)) {
+        // grab each contact (i.e. line in file) and render attributes in the UI
+        // we need a new row for every 4 contacts to ensure proper UI layout
+        if (($count > 0) && ($count % 4 == 0)) {
+            echo '</div>\n';
+            echo '<div class="row">';
+        }
+        $count++;
+        $contact = fgetcsv($csvFileHandler); 
         echo '<div class="col-xs-6 col-md-3">';
         echo '<div class="thumbnail">';
-        echo '<img src="https://pbs.twimg.com/profile_images/783345725547708416/jV-j2pxp_400x400.jpg" />';
+        echo '<img src="' . $contact[$img] . '" />';
         echo '<div class="caption">';
-        echo '<h4 class="media-heading">Alena Holligan</h4>';
-        echo '<a href="http://sketchings.com" target="_blank">sketchings.com</a>';
+        echo '<h4 class="media-heading">' . $contact[$first] . ' ' . $contact[$last] . '</h4>';
+        echo '<a href="http://' . $contact[$website] . '" target="_blank">' . $contact[$website] . '</a>';
         echo '<br />';
-        echo 'Twitter: <a href="https://twitter.com/sketchings" target="_blank">@sketchings</a>';
+        echo 'Twitter: <a href="https://twitter.com/' . $contact[$twitter] . '" target="_blank">@' . $contact[$twitter] . '</a>';
         echo '</div>';
         echo '</div>';
         echo '</div>';
-echo '</div>';
+    }
+    echo '</div>';
+    fclose($csvFileHandler);
+}
+
 require 'inc/footer.php';
